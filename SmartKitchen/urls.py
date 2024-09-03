a00f59d0ee29abebdfa5f1f1402372bca00f29ab
Ansari-5 <102139444+Ansari-5@users.noreply.github.com>
2024-09-03 22:21:28 +0530

    all

--- SmartKitchen/urls.py
+++ SmartKitchen/urls.py
@@ -16,8 +16,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('SmartKitchen_App.urls')),
-]
+
+
+    # your URL patterns
+
+
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
