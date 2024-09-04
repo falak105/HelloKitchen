14dc1a5fb2e6d3c09c076231653085de26ce66bf
GMust211 <george.thomas@msds.christuniversity.in>
2024-09-04 20:35:07 +0530

    date :4 push

--- SmartKitchen_App/urls.py
+++ SmartKitchen_App/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
-from .views import  home
+from .views import  home,about,contact,booking,menu,service,team,testimonial
 
 urlpatterns = [
     path('', home, name='index'),
+    path('about/',about, name='about'),
+    path('contact/',contact, name='contact'),
+    path('booking/',booking, name='booking'),
+    path('menu/',menu, name='menu'),
+    path('service/',service, name='service'),
+    path('team/',team, name= 'team'),
+    path('testimonial/',testimonial, name= 'testimonial')
 ]
--- SmartKitchen_App/views.py
+++ SmartKitchen_App/views.py
@@ -3,3 +3,18 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'index.html')
+def about(request):
+    return render(request,'about.html')
+def contact(request):
+    return render(request,'contact.html')
+def booking(request):
+    return render(request,'booking.html')
+def menu(request):
+    return render(request, 'menu.html')
+def service(request):
+    return render(request, 'service.html')
+def team(request):
+    return render(request, 'team.html')
+def testimonial(request):
+    return render(request, 'testimonial.html')
+
